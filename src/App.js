c3ba46ba3afa3b050fbf1e117d2281d41ad6d2c7
quonly <amornthep.mos07@gmail.com>
2023-06-26 17:55:42 +0700

    ep73 5:22

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+]
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
-  );
+  )
 }
 
-export default App;
+function Logo() {
+  return <h1>🏝 Far Away 👜</h1>
+}
+
+function Form() {
+/**
+ * Controll Element
+ * 1. Create a piece of state.
+ * 2. use that state as a value of the input field.
+ * 3. listen for the change event.
+ */
+
+  const [description,setDescription] = useState('')
+
+  function handlerSubmit(e){
+    e.preventDefault()
+  }
+  
+  return (
+    <form className="add-form" onSubmit={handlerSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        {Array.from({length: 20}, (_, i) => i+1).map(num=><option value={num} key={num}>{num}</option>)}
+      </select>
+      <input type="text" placeholder="Item..." value={description} onChange={(e)=>setDescription(e.target.value)} />
+      <button>Add</button>
+    </form>
+  )
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map(item => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration:'line-through'} : {}}>{item.quantity} {item.description} </span>
+      <button>❌</button>
+    </li>
+  )
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜 You have X items on your lsit, and you already packed X(X%)</em>
+    </footer>
+  )
+}
